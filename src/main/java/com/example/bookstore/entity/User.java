5c1072a99e83a4e6df49375f0b2df5cd6aad9072
Giaanh0509 <ngotthe0509@gmail.com>
2024-08-22 11:27:02 +0700

    add user entity

---
+++ src/main/java/com/example/bookstore/entity/User.java
@@ -0,0 +1,27 @@
+package com.example.bookstore.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "username", length = 50)
+    private String username;
+
+    @Column(name = "password", length = 250)
+    private String password;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+}
