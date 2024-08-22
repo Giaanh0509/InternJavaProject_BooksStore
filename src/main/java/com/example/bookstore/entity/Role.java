968227b9532b96ae4cff78a9a711673db3c9a7db
Giaanh0509 <ngotthe0509@gmail.com>
2024-08-22 14:08:01 +0700

    add configdatabase

--- src/main/java/com/example/bookstore/entity/Role.java
+++ src/main/java/com/example/bookstore/entity/Role.java
@@ -2,6 +2,8 @@ package com.example.bookstore.entity;
 
 import jakarta.persistence.*;
 
+@Entity
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,4 +15,36 @@ public class Role {
 
     @OneToOne(mappedBy = "role")
     private User user;
+
+    public Role() {
+    }
+
+    public Role(String name, User user) {
+        this.name = name;
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/example/bookstore/entity/User.java
+++ src/main/java/com/example/bookstore/entity/User.java
@@ -28,4 +28,72 @@ public class User {
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "role_id", referencedColumnName = "id")
     private Role role;
+
+    public User() {
+    }
+
+    public User(String username, String password, String address, String email, String phoneNumber, Role role) {
+        this.username = username;
+        this.password = password;
+        this.address = address;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
